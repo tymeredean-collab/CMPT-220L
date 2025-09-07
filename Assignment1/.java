3a7367581d22d8152150914d479e8ee47bb36be7
Tymere Dean <tymeredean@gmail.com>
2025-09-06 23:30:47 -0400

    Update .java

--- Assignment1/.java
+++ Assignment1/.java
@@ -4,7 +4,7 @@ public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("How old are you? ");
     String age = sc.nextLine();
-    System.out.println(22);
+    System.out.println(age);
     int num1 = 15; 
     int num2 = 25;
     System.out.println(num1 + num2); // What I learned: How to successfully add two integers and how to ask the user a statement. 
