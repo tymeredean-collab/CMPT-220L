fe1f76ab277c14906a746bd7cc776159e0fc2ab1
Tymere Dean <tymeredean@gmail.com>
2025-10-12 23:55:04 -0400

    Update functions.java

--- Assignment 5/functions.java
+++ Assignment 5/functions.java
@@ -1,1 +1,30 @@
+import java.util.Scanner
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in)
+        System.out.println("Enter a sentence:");
+        String sentence = sc.nextLine();
 
+        int vowels = countVowels(sentence);
+        System.out.println("Number of vowels: " + vowels);
+
+        sc.close();
+    }
+
+    public static int countVowels(String text) {
+        int count = 0;
+        String lower = text.toLowerCase();
+        for (int i = 0; i < lower.length(); i++) {
+            char c = lower.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+        
+    
+    }
+   
+}
