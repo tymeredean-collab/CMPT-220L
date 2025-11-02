7821dc1c71e5a68038881385647164630563e74f
Tymere Dean <tymeredean@gmail.com>
2025-11-02 18:06:42 -0500

    Create Enemy.java

---
+++ Assignment 7/Enemy.java
@@ -0,0 +1,12 @@
+public class Enemy {
+int health;
+int damage;
+
+public Enemy(int h, int d){
+    health = h;
+    damage = d;
+}
+void attack(){
+    System.out.println("the enemy attacks");
+}
+}
